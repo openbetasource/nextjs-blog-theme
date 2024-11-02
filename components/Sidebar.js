045173db3ffa3614897439053f8222e62bfcae55
Open Beta Source <openbetasource@gmail.com>
2024-11-02 12:25:31 +0100

    summ

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 h-full text-black fixed flex items-center justify-center">
+    <aside className="w-64 h-full text-white fixed flex items-center justify-center">
       <nav className="flex flex-col space-y-4">
-        <Link href="/gallery" className="hover:text-gray-600">
+        <Link href="/gallery" className="hover:text-gray-300">
           Gallery
         </Link>
-        <Link href="/products" className="hover:text-gray-600">
+        <Link href="/products" className="hover:text-gray-300">
           Products
         </Link>
-        <Link href="/information" className="hover:text-gray-600">
+        <Link href="/information" className="hover:text-gray-300">
           Information
         </Link>
       </nav>
